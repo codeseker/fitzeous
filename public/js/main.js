6eb0d877d2dfbf02bfbd7609a36c80a7526ab208
Ayush Jangid <ayushjangid441@gmail.com>
2023-04-20 23:32:57 +0530

    logout added

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,39 @@
-console.log("heyy");
 let result = document.getElementById("result");
 let login = document.getElementById("login");
 let signup = document.getElementById('signup');
 
 let authToken = localStorage.getItem("auth-token");
-console.log(authToken)
-if(!authToken){
-    window.location = ("/signup.html")
+if (!authToken) {
+    window.location = ("/login.html")
 }
-
-if (authToken) {
-    login.innerText = "Logout"
-    let cnt = 0;
-    // fetch the blog according to categories
-    const getBlogs = async (tag) => {
-        result.innerHTML = "";
-        const data = { tag: tag };
-        fetch("/api/v1/health/searchByTag", {
-            method: "POST", // or 'PUT'
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": authToken
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                for (let element of data) {
-                    if (cnt == 2) {
-                        break;
-                    }
-                    let str = `<div class="col-lg-6 col-md-6">
+else {
+    if (login.innerText == "Logout") {
+        localStorage.clear();
+        window.location = ("/login.html");
+    }
+    else {
+        login.innerText = "Logout"
+        let cnt = 0;
+        // fetch the blog according to categories
+        const getBlogs = async (tag) => {
+            result.innerHTML = "";
+            const data = { tag: tag };
+            fetch("/api/v1/health/searchByTag", {
+                method: "POST", // or 'PUT'
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": authToken
+                },
+                body: JSON.stringify(data),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    for (let element of data) {
+                        if (cnt == 2) {
+                            break;
+                        }
+                        let str = `<div class="col-lg-6 col-md-6">
                 <div class="latest_news">
                     <div class="news_img">
                         <a href="blog-details.html"><img src="${element.image}" alt=""></a> 
@@ -42,16 +44,20 @@ if (authToken) {
                     <a href="blog-details.html?id=${element._id}" class="find_btn">Read more</a>
                 </div>
             </div>`
-                    result.innerHTML += str;
-                    cnt++;
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
+                        result.innerHTML += str;
+                        cnt++;
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
 
+        }
+        getBlogs("inspiration");
     }
-
-    getBlogs("inspiration");
 }
+
+
+
+
 
